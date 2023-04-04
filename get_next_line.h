a4429b5371b5a887ab26101d76691d0eed0a9278
Clocon <luiste49@gmail.com>
2023-04-04 02:04:51 +0200

    Back to original versio
    This project is more complicate to implement for other projects, I prefer to keep this original version of the project and adapt get_next_line independently in future projects where it is necessary for me.

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lumorale <lumorale@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 12:34:33 by lumorale          #+#    #+#             */
-/*   Updated: 2023/04/03 12:24:08 by lumorale         ###   ########.fr       */
+/*   Updated: 2023/01/16 15:56:40 by lumorale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,19 @@
 #  define BUFFER_SIZE 42
 # endif
 
-# include "../libft/includes/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
 
 char	*get_next_line(int fd);
 char	*ft_next_line(char *str);
 char	*ft_get_line(char *str);
 char	*ft_read_file(int fd, char *str);
-
+void	*ft_calloc(size_t nmemb, size_t size);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_strjoin(char *s1, char *s2, int n);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *str);
 
 #endif
